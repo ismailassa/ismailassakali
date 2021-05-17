e586756a0dc9521eaa2adeb3d01e58c515aa7247
Ismail Assakali <ismail.assakali@student.hogent.be>
2021-05-17 14:28:29 +0200

    static docs

---
+++ precache-manifest.bd92c6ab1b0e35d8f0f66f688a5c7526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1958024046544ecf18cf908ad2d1bc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e984eee34bfb9be6297a",
+    "url": "/static/css/main.d11511b8.chunk.css"
+  },
+  {
+    "revision": "53259ba8cee21d550180",
+    "url": "/static/js/2.bcbc9aba.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.bcbc9aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e984eee34bfb9be6297a",
+    "url": "/static/js/main.050abb4e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
